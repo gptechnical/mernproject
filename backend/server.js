d38b628e7dc05a2400cd1dd395e8a69a50d5e2aa
anjali-prajapati23 <parjapatianjali8587@gmail.com>
2025-06-27 12:47:22 +0530

    updte all url backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,16 @@ const adminRoute = require("./router/admin-router");
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+origin: (origin, callback) => {
+  const allowedOrigins = [
+     "http://localhost:5173",
+     "http://localhost:4173",
+     "https://codegptech.shop",
+     "https://www.codegptech.shop",
+  ];
+  const isAllowed = allowedOrigins.include(origin);
+  callback(null, isAllowed ? origin : false );
+},
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
