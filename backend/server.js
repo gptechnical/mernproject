416df07812dfefdd45e7794ef75655193fcc06ec
anjali-prajapati23 <parjapatianjali8587@gmail.com>
2025-06-28 01:04:54 +0530

    origin update

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,24 @@ const serviceRoute = require("./router/service-router");
 const adminRoute = require("./router/admin-router");
 
 app.use(express.json());
-const corsOptions = {
-origin: (origin, callback) => {
-  const allowedOrigins = [
-     "http://localhost:5173",
-     "http://localhost:4173",
-     "https://codegptech.shop",
-     "https://www.codegptech.shop",
-  ];
-  const isAllowed = allowedOrigins.include(origin);
-  callback(null, isAllowed ? origin : false );
-},
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-};
+// const corsOptions = {
+// origin: (origin, callback) => {
+//   const allowedOrigins = [
+//      "http://localhost:5173",
+//      "http://localhost:4173",
+//      "https://codegptech.shop",
+//      "https://www.codegptech.shop",
+//   ];
+//   const isAllowed = allowedOrigins.include(origin);
+//   callback(null, isAllowed ? origin : false );
+// },
+//   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: "*"
+}));
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
 app.use("/api/data", serviceRoute);
