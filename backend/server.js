73dae0a4555e3d813a668045a2690725b7b2b5e6
anjali-prajapati23 <parjapatianjali8587@gmail.com>
2025-06-28 14:10:51 +0530

    update code api

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ const corsOptions = {
     const allowedOrigins = [
       "http://localhost:5173/",
       "http://localhost:4173/",
-      "https://codegptech.shop/",
       "https://www.codegptech.shop/",
     ];
     const isAllowed = allowedOrigins.includes(origin);
