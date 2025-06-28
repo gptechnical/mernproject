158d1ca45f588a711e8601b51af1ff04eee651ee
anjali-prajapati23 <parjapatianjali8587@gmail.com>
2025-06-28 14:24:15 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,47 @@ const serviceRoute = require("./router/service-router");
 const adminRoute = require("./router/admin-router");
 
 app.use(express.json());
-const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      "http://localhost:5173/",
-      "http://localhost:4173/",
-      "https://www.codegptech.shop/",
-    ];
-    const isAllowed = allowedOrigins.includes(origin);
-    callback(null, isAllowed ? origin : false );
-  },
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     const allowedOrigins = [
+//       "http://localhost:5173/",
+//       "http://localhost:4173/",
+//       "https://www.codegptech.shop/",
+//     ];
+//     const isAllowed = allowedOrigins.includes(origin);
+//     callback(null, isAllowed ? origin : false );
+//   },
+//   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "http://localhost:4173/",
+  "https://www.codegptech.shop/",
+];
+
+// Use the cors middleware with specific options
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        // callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST", "GET", "PUT", "PATCH", "REQUEST", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.get("/", async (req, res) => {
+  return res.status(200).send("Welcometo CodeGPT");
+});
+
+
+// app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
