1032787bb08eb23c415345bd830bc9d3c79086bd
anjali-prajapati23 <parjapatianjali8587@gmail.com>
2025-07-06 12:00:00 +0530

    update cde

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
-      "http://localhost:5173/",
-      "http://localhost:4173/",
-      "https://codegptech.shop/",
+      "http://localhost:5173",
+      "https://codegptech.shop",
+      "https://www.codegptech.shop",
     ];
     const isAllowed = allowedOrigins.includes(origin);
     callback(null, isAllowed ? origin : false );
