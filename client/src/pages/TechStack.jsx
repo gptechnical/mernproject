fee95f0e39732334f192641b25dd4bb52805fd3a
anjali-prajapati23 <parjapatianjali8587@gmail.com>
2025-11-15 15:13:12 +0530

    tech img update

--- client/src/pages/TechStack.jsx
+++ client/src/pages/TechStack.jsx
@@ -1,19 +1,32 @@
 import "../assets/css/Button.css";
 import { FaArrowRight } from "react-icons/fa6";
+import Tech1 from "../assets/images/technology/tech1.png";
+import Tech2 from "../assets/images/technology/tech2.png";
+import Tech3 from "../assets/images/technology/tech3.png";
+import Tech4 from "../assets/images/technology/tech4.png";
+import Tech5 from "../assets/images/technology/tech5.png";
+import Tech6 from "../assets/images/technology/tech6.png";
+import Tech7 from "../assets/images/technology/tech7.png";
+import Tech8 from "../assets/images/technology/tech8.png";
+import Tech9 from "../assets/images/technology/tech9.png";
+import Tech10 from "../assets/images/technology/tech10.png";
+import Tech11 from "../assets/images/technology/tech11.png";
+import Tech12 from "../assets/images/technology/tech12.png";
+
 
 const TECHS = [
-  { id: 1, name: 'Angular JS', logo: '/src/assets/images/technology/tech1.png' },
-  { id: 2, name: 'React JS', logo: '/src/assets/images/technology/tech2.png' },
-  { id: 3, name: 'HTML5', logo: '/src/assets/images/technology/tech3.png' },
-  { id: 4, name: 'CSS3', logo: '/src/assets/images/technology/tech4.png' },
-  { id: 5, name: 'JavaScript', logo: '/src/assets/images/technology/tech5.png' },
-  { id: 6, name: 'Bootstrap', logo: '/src/assets/images/technology/tech6.png' },
-  { id: 7, name: 'jQuery', logo: '/src/assets/images/technology/tech7.png' },
-  { id: 8, name: 'Wordpress', logo: '/src/assets/images/technology/tech8.png' },
-  { id: 9, name: 'PHP', logo: '/src/assets/images/technology/tech9.png' },
-  { id: 10, name: 'My SQL', logo: '/src/assets/images/technology/tech10.png' },
-  { id: 11, name: 'Node JS', logo: '/src/assets/images/technology/tech11.png' },
-  { id: 12, name: 'MongoDB', logo: '/src/assets/images/technology/tech12.png' }
+  { id: 1, name: 'Angular JS', logo: Tech1 },
+  { id: 2, name: 'React JS', logo: Tech2 },
+  { id: 3, name: 'HTML5', logo: Tech3 },
+  { id: 4, name: 'CSS3', logo: Tech4 },
+  { id: 5, name: 'JavaScript', logo: Tech5 },
+  { id: 6, name: 'Bootstrap', logo: Tech6 },
+  { id: 7, name: 'jQuery', logo: Tech7 },
+  { id: 8, name: 'Wordpress', logo: Tech8 },
+  { id: 9, name: 'PHP', logo: Tech9 },
+  { id: 10, name: 'My SQL', logo: Tech10 },
+  { id: 11, name: 'Node JS', logo: Tech11 },
+  { id: 12, name: 'MongoDB', logo: Tech12 }
 ];
 
 function TechStack() {
